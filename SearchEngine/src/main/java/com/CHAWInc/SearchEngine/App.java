4cfff8965fab2a722ea77dd468ffd1fab74e2924
WALTER GOES <tinhogoes@gmail.com>
2020-02-19 18:34:32 -0500

    5th commit,started to add some search methods and psuedo code

--- SearchEngine/src/main/java/com/CHAWInc/SearchEngine/App.java
+++ SearchEngine/src/main/java/com/CHAWInc/SearchEngine/App.java
@@ -1,3 +1,4 @@
+// import the index file (need name)
 package com.CHAWInc.SearchEngine;
 
 /**
@@ -8,8 +9,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         
-        System.out.println("Testing netbeans commit");
+        //we need a path to our .txt file
+        
+        
+        //need to make an arraylist
+        
+        //String searchFile = getInput();  //this will be the method??
+        
+        //need to display the search results somehow
+        
+     
     }
+
+        //user input method, doing a scanner i think is the best way
+        // feel free to change or give suggestions to improve
+        public static String getInput() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your search: ")
+            String nameTitle = sc.nextLine();
+            sc.close();
+            
+            return titlename;
+        }
+
 }
