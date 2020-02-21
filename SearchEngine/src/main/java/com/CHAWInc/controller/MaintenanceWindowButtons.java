9160325a1fb10ecbec0f3e71fffc02f80554b56d
Charles Sanders <csanders33@hawkmail.hccfl.edu>
2020-02-21 16:31:54 -0500

    Add button method stubs to MaintenanceWindowButtons

--- SearchEngine/src/main/java/com/CHAWInc/controller/MaintenanceWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/MaintenanceWindowButtons.java
@@ -1,5 +1,34 @@
 package com.CHAWInc.controller;
 
-public class MaintenanceWindowButtons {
+//Assigned to Charles Sanders
 
+public class MaintenanceWindowButtons {
+	
+	public static void clickAddFile() {
+	
+		//TODO open File window
+		//Add to FIle Name list
+		
+	}
+	
+	public static void clickRemoveSelectedFiles() {
+		
+		//TODO display Yes/No/Cancel popup with
+		//text "Remove these files from the index?" 
+		// < FIle Name >
+	}
+	
+	public static void clickRebuildOutOfDate() {
+		
+		//TODO add code to rebuild the index
+		
+	}
+	
+	public static void clickResetWindows() {
+		
+		//TODO add code to move 1 or both windows
+		// to the center of the screen
+		
+	}
+	
 }
