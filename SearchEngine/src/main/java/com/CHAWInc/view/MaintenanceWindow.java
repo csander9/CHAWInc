d7ff91a8124d6a52b7ae06d4445799d0245e81d2
Tboy1492 <Tboy1492@ReturnOfTboy.fios-router.home>
2020-02-21 11:29:37 -0500

    Created starting code for Maintenance Window GUi method is "MaintGUI"

--- SearchEngine/src/main/java/com/CHAWInc/view/MaintenanceWindow.java
+++ SearchEngine/src/main/java/com/CHAWInc/view/MaintenanceWindow.java
@@ -1,5 +1,18 @@
 package com.CHAWInc.view;
+import javax.swing.*; 
 
 public class MaintenanceWindow {
 
+	public static void MaintGUI (String[] args) {
+		JFrame maint = new JFrame();//creating instance of JFrame  	
+		maint.setSize(600,800); //600 width and 800 height  
+		maint.setVisible(true); //making the window visible
+		
+		// Creating exit button for the maintenance window.
+			
+		JButton bye = new JButton("Exit Maintenance");
+		bye.setBounds(500, 500, 100, 40);// Setting location to 500,500 at 100x40 size.
+		
+	}
 }
+
