0be16bd8d6801b3a96c255e17bb46a51773e5749
Hung Truong <2deflectu@gmail.com>
2020-02-21 11:24:39 -0500

    added methods to when Search, Maintenance, About buttons are clicked

--- SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
@@ -3,6 +3,8 @@ package com.CHAWInc.controller;
 public class SearchWindowButtons {
     public static void on_Click_Search_Button() {
         // TODO add code to search with radio button selection
+        //              and return search results to main window
+
     }
     public static void on_Click_Maintenance_Button() {
         // TODO add code to open up Maintenance Window
