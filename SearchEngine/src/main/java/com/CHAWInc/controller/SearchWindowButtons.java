786356f09b1b8998af3d8c12fbc92eb2611920b0
Hung Truong <2deflectu@gmail.com>
2020-02-20 23:41:43 -0500

    Accomplished: Radio Buttons with text
    Pending: Clickable action buttons with text for "Search", "Maintenance", and "About"

--- SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
@@ -1,5 +1,49 @@
 package com.CHAWInc.controller;
 
-public class SearchWindowButtons {
+import java.awt.FlowLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
+public class SearchWindowButtons extends JFrame {
+
+
+
+    public SearchWindowButtons() {
+        //Search button
+        JButton searchButton = new JButton("Search");
+        //Radio buttons
+        JRadioButton radioButton1 = new JRadioButton("All of the Search Terms", true);
+        JRadioButton radioButton2 = new JRadioButton("Any of the Search Terms");
+        JRadioButton radioButton3 = new JRadioButton("Exact Phrase");
+
+        ButtonGroup radioGroup = new ButtonGroup();
+        radioGroup.add(radioButton1);
+        radioGroup.add(radioButton2);
+        radioGroup.add(radioButton3);
+
+        setLayout(new FlowLayout());
+
+        add(radioButton1);
+
+        add(radioButton2);
+
+        add(radioButton3);
+        //Maintenance button
+        JButton maintenanceButton = new JButton("Maintenance");
+        //About button
+        JButton aboutButton = new JButton("About");
+
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SearchWindowButtons().setVisible(true);
+            }
+        });
+    }
 }
+
