54982f4c283a457c45cd78ba1e0b75f7c522879d
Tboy1492 <Tboy1492@ReturnOfTboy.fios-router.home>
2020-02-21 12:37:24 -0500

    Succeeded in displaying exit button.

--- SearchEngine/src/main/java/com/CHAWInc/view/MaintenanceWindow.java
+++ SearchEngine/src/main/java/com/CHAWInc/view/MaintenanceWindow.java
@@ -3,16 +3,18 @@ import javax.swing.*;
 
 public class MaintenanceWindow {
 
-	public static void MaintGUI (String[] args) {
-		JFrame maint = new JFrame();//creating instance of JFrame  	
-		maint.setSize(600,800); //600 width and 800 height  
+	public static void main(String[] args) {
+		// Creating the JFrame and settings its properties.
+		JFrame maint = new JFrame();
+		maint.setSize(800,600);  
+		maint.setLayout(null);
 		maint.setVisible(true); //making the window visible
 		
 		// Creating exit button for the maintenance window.
 			
 		JButton bye = new JButton("Exit Maintenance");
-		bye.setBounds(500, 500, 100, 40);// Setting location to 500,500 at 100x40 size.
-		
+		bye.setBounds(500, 500, 200, 40);// Setting location to 500,500 at 100x40 size.
+		maint.add(bye);
 	}
 }
 
