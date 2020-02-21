164833e5fe6242fc3cd33b4a560721cd080fc05e
retlaw86 <tinhogoes@gmail.com>
2020-02-20 20:36:07 -0500

    commit 6

--- SearchEngine/src/main/java/com/CHAWInc/view/SearchWindow.java
+++ SearchEngine/src/main/java/com/CHAWInc/view/SearchWindow.java
@@ -99,4 +99,7 @@ public class SearchWindow extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
+
+//ADD EXIT BUTTON
+    //ADD MAINTENANCE WINDOW
 }
