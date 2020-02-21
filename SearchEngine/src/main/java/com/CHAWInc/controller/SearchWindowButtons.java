6ca8fa483b73623f259f6db015c4e68319121488
Hung Truong <2deflectu@gmail.com>
2020-02-21 12:33:49 -0500

    added more stubs

--- SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
@@ -4,12 +4,15 @@ public class SearchWindowButtons {
     public static void on_Click_Search_Button() {
         // TODO add code to search with radio button selection
         //              and return search results to main window
+        // display_Search_Results();
 
     }
     public static void on_Click_Maintenance_Button() {
         // TODO add code to open up Maintenance Window
+        // open_Maintenance_Window();
     }
     public static void on_Click_About_Button() {
         // TODO add code to open up About window
+        // open_About_Window();
     }
 }
