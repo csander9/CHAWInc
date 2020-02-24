e8b5ae9a378218ac77ce6d4a20c41e4a619a3ab2
twodeflect <60677286+twodeflect@users.noreply.github.com>
2020-02-23 20:43:48 -0500

    Add files via upload

--- SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
@@ -1,5 +1,22 @@
 package com.CHAWInc.controller;
+import com.CHAWInc.view.*;
 
+// assigned to Hung Truong
 public class SearchWindowButtons {
+    public static void onClickSearchButton() {
+        // add code to search with radio button selection
+        //              and return search results to main window
+        // display_Search_Results();
 
+    }
+
+    public static void onClickMaintenanceButton() {
+        // add code to open up Maintenance Window
+        MaintenanceWindow p = new MaintenanceWindow();
+        p.main();
+    }
+    public static void onClickAboutButton() {
+        // add code to open up About window
+        // open_About_Window();
+    }
 }
