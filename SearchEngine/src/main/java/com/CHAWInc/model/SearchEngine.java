6603acf29e7b9fe65492fc562f3b15516e243cc2
csander9 <60243913+csander9@users.noreply.github.com>
2020-02-23 20:46:11 -0500

    Add files via upload

--- SearchEngine/src/main/java/com/CHAWInc/model/SearchEngine.java
+++ SearchEngine/src/main/java/com/CHAWInc/model/SearchEngine.java
@@ -1,36 +1,24 @@
-// import the index file (need name)
 package com.CHAWInc.model;
 
+import com.CHAWInc.view.*;
+
 /**
- * Hello world!
+ * Search Engine Program Project #3.
+ * 
+ * @authors Charles Sanders, Hung Truong, Walter Goes, Anthony Baudoin
  *    
  */
-public class SearchEngine 
-{
-    public static void main( String[] args )
-    {
+public class SearchEngine {
+	
+    public static void main(String args[]) {
+
+        //Create and display the main Search Engine window 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+           public void run() {
+                new SearchWindow();
+            }
+        });
         
-        //we need a path to our .txt file
-        
-        
-        //need to make an arraylist
-        
-        //String searchFile = getInput();  //this will be the method??
-        
-        //need to display the search results somehow
-        
-     
     }
-
-        //user input method, doing a scanner i think is the best way
-        // feel free to change or give suggestions to improve
-        public static String getInput() {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your search: ")
-            String nameTitle = sc.nextLine();
-            sc.close();
-            
-            return titlename;
-        }
-
 }
+
