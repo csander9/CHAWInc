07fb75a8d455cf0c4419547567848b851b472e21
retlaw86 <tinhogoes@gmail.com>
2020-02-24 13:00:08 -0500

    added some of hungs branch

--- SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
+++ SearchEngine/src/main/java/com/CHAWInc/controller/SearchWindowButtons.java
@@ -1,5 +1,23 @@
 package com.CHAWInc.controller;
+import javax.swing.JOptionPane;
+
 
 public class SearchWindowButtons {
-
+    public static void onClickSearchButton() {
+        // add code to search with radio button selection
+        //              and return search results to main window
+        // display_Search_Results();
+    }
+    public static void onClickAboutButton() {
+        Object[] options = {"OK"};
+        JOptionPane.showOptionDialog(null,
+                "Search Engine 1.0\n" + "Group: CHAWInc\n" +
+                        "Project 3\n" +
+                        "SP/20 COP-2805C\n","Search Engine",
+                JOptionPane.PLAIN_MESSAGE,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]);
+    }
 }
