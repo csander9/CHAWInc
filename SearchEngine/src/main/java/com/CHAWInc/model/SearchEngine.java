86b7738705c6cfb65b3f9fd945ccd1b826a6571e
Tboy1492 <AnthonyBaudoin0@gmail.com>
2020-03-11 21:06:38 -0400

    Added note regarding SearchEngine.java created temporary string assignment to clear an error in getInput() method.

--- SearchEngine/src/main/java/com/CHAWInc/model/SearchEngine.java
+++ SearchEngine/src/main/java/com/CHAWInc/model/SearchEngine.java
@@ -1,6 +1,8 @@
 // import the index file (need name)
 package com.CHAWInc.model;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *    
@@ -19,6 +21,9 @@ public class SearchEngine
         
         //need to display the search results somehow
         
+    	// APB- If we are using getInput() to look through the list for a 
+    	// specific named file that should work. 
+    	
      
     }
 
@@ -26,11 +31,17 @@ public class SearchEngine
         // feel free to change or give suggestions to improve
         public static String getInput() {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your search: ")
+            System.out.println("Enter your search: ");
             String nameTitle = sc.nextLine();
             sc.close();
             
-            return titlename;
+            // temporary assignment to remove errors for the moment
+            String titlename = nameTitle;
+            return titlename; 
+            /* APB - this titlename variable seems undefined,
+               What is it we want it to return exactly, an input from the user?
+               I am setting a temporary variable to remove the error flag. 
+            */
         }
 
 }
